4412aadee364224e955cf1567083001a85dbc41e
Stanislav <quah.fy95@gmail.com>
2020-04-22 20:15:49 +0100

    Revert "m"
    This reverts commit 1c3197bb48dcfe97c261235f53b28065b50beed7.

--- compiler/src/un_cps.ml
+++ compiler/src/un_cps.ml
@@ -8,7 +8,8 @@ let of_flambda_expr (expr : Flambda.Expr.t) =
   match descr with
   | Let_cont let_cont ->
     begin match let_cont with
-      | Non_recursive { handler = _; num_free_occurrences = _ } -> 
+      | Non_recursive { handler = _; num_free_occurrences = _ } ->
+        Non_recursive.
         ()
       | Recursive _ -> ()
     end
